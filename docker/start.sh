41ff7a0903f4a507b157e9f39226f70f71e87015
David Monschein <david.monschein@novatec-gmbh.de>
2019-04-01 15:10:06 +0200

    Fixed docker image.

--- docker/start.sh
+++ docker/start.sh
@@ -8,6 +8,6 @@ git pull
 gradle bootJar
 
 # running
-cd cd /etc/pipeline/PCM-Docker/org.pcm.automation.interface/build/libs/
+cd /etc/pipeline/PCM-Docker/org.pcm.automation.interface/build/libs/
 
 java -jar pcm-analysis-interface-0.1.0.jar --javaPath="java" --eclipsePath="/etc/eclipse/eclipse/"
